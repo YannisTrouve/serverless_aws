d8d39bff744ec06c7f9ed72ccb45603ccd17c250
YannisTrouve <45897155+YannisTrouve@users.noreply.github.com>
2020-11-16 11:23:23 +0100

    Tous les tests sont passés

--- billing.js
+++ billing.js
@@ -14,7 +14,7 @@ export const main = handler(async (event, context) => {
     source,
     amount,
     description,
-    currency: "usd"
+    currency: "eur"
   });
   return { status: true };
 });
